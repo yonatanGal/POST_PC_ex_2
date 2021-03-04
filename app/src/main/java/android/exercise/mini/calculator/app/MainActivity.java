b9c1ab19bf9fa87e548d29359ec75d02d2726945
Re'em Halamish <reemh@google.com>
2021-03-04 20:20:05 +0200

    Added TODOs in MainActivity

--- app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
+++ app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
@@ -1,14 +1,43 @@
 package android.exercise.mini.calculator.app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 public class MainActivity extends AppCompatActivity {
+
+  @VisibleForTesting
+  public SimpleCalculator calculator;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (calculator == null) {
+      calculator = new SimpleCalculatorImpl();
+    }
+
+    /*
+    TODO:
+    - find all views
+    - initial update main text-view based on calculator's output
+    - set click listeners on all buttons to operate on the calculator and refresh main text-view
+     */
+  }
+
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+    // todo: save calculator state into the bundle
+  }
+
+  @Override
+  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    // todo: restore calculator state from the bundle, refresh main text-view from calculator's output
   }
 }
